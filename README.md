083fccefe3f6ef33986a2cb1dd3cadd61b798d33
hyeonga493 <127083504+hyeonga493@users.noreply.github.com>
2024-05-10 11:59:20 +0900

    Build : 프로젝트 초기화 및 기본 설정 파일 (#1)
    멀티 모듈 프로젝트 초기화

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hyeongarl
+
+23.05.05. add new branch (feature/start)
---
+++ api/src/test/java/com/hyeongarl/ApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hyeongarl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+    @Test
+    void contextLoads(){
+    }
+}
