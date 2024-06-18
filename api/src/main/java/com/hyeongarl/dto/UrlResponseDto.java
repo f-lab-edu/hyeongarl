fc86c04710fbee72370b52e6b7e75f1dbe8df8a8
hyeonga493 <hyeonga4349@naver.com>
2024-06-18 21:02:10 +0900

    [#8] feat: Url Entity & Dto & Repository
    - Created Url Entity
    - Created UrlRequestDto
    - Created UrlResponseDto
    - Created UrlRepository

--- api/src/main/java/com/hyeongarl/dto/UrlResponseDto.java
+++ api/src/main/java/com/hyeongarl/dto/UrlResponseDto.java
@@ -1,5 +1,7 @@
 package com.hyeongarl.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hyeongarl.entity.Url;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,15 +10,31 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class UrlResponseDto {
     private Long urlId;
     private String url;
     private String urlTitle;
     private String urlDescription;
     private Long categoryId;
-    private LocalDateTime urlRegDate;
-    private LocalDateTime urlUpdateDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime urlRegdate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime urlUpdate;
+
+    public static UrlResponseDto fromEntity(Url url) {
+        return UrlResponseDto.builder()
+                .urlId(url.getUrlId())
+                .url(url.getUrl())
+                .urlTitle(url.getUrlTitle())
+                .urlDescription(url.getUrlDescription() == null ? null : url.getUrlDescription())
+                .categoryId(url.getCategoryId() == null ? null : url.getCategoryId())
+                .urlRegdate(url.getUrlRegdate() == null ? null : url.getUrlRegdate())
+                .urlUpdate(url.getUrlUpdate() == null ? null : url.getUrlUpdate())
+                .build();
+    }
 }
